b177a0ce04d10478bb6961cd75a8dd51b13bdfdf
Lauany Reis da Silva <laulauany@hotmail.com>
2020-08-25 12:35:57 -0300

    Update d_CaixeiroViajante.rb
    Atualização da documentação

--- Questao3.17/d_CaixeiroViajante.rb
+++ Questao3.17/d_CaixeiroViajante.rb
@@ -1,5 +1,4 @@
-# 3.17 d) Implemente o algoritmo e aplique-o a instâncias do quebra-cabeça de oito peças e do caixeiroviajante.
-# Compare o desempenho do algoritmo ao desempenho da busca de custo uniforme e comente seus resultados.
+# 3.17 d) Implemente o algoritmo e aplique-o a instâncias (...) do caixeiroviajante.
 # Adapter: Lauany Reis da Silva
 
 City = Struct.new(:x, :y, :name)
@@ -14,7 +13,7 @@ end
 def calculate_distance(from, to)
   Math.sqrt((to.x-from.x)**2 + (to.y-from.y)**2).to_i
 end
- 
+# Algoritmo de busca  
 def go_route(route, routes)
   if route.cities.length == CITIES.length
     route.distance += calculate_distance(route.cities[-1], route.cities[0])
@@ -24,6 +23,7 @@ def go_route(route, routes)
   end
  
   for city in CITIES - route.cities
+    # Algoritmo A*
     new_route = Route.new(route.cities + [city], route.distance + calculate_distance(route.cities.last, city))
     go_route(new_route, routes)
   end
@@ -39,7 +39,7 @@ def r(n=10000)
   (rand*n).to_i
 end
  
-for name in ["Python", "Java", "PHP", "C", "Ruby", "BrainF***ck", "JavaScript"]
+for name in ["Python", "Java", "PHP", "C", "Ruby", "BrainF*ck", "JavaScript"]
   add_city(r, r, name)
 end
  
@@ -49,12 +49,12 @@ puts "Exibindo as 10 melhores rotas!"
 best_routes.each_with_index do |route, i|
   puts "\n"*2
   puts "Rota ##{i+1}"
-  puts " Distância percorrida: #{route.distance}"
+  puts " Distancia percorrida: #{route.distance}"
   puts " Cidades:"
  
   last_city = nil
   distance  = 0
-  puts "  %10s | %07s, %7s | %10s | %s" % [ "Cidade", "Cord X", "Cord Y", "Distância total", "Distancia da última cidade"]
+  puts "%10s | %07s, %7s | %10s | %s" % [ "     Cidades", "Cord X", "Cord Y", "Distancia", "Distancia da ultima cidade"]
   route.cities.each do |city|
     distance_between_cities = (last_city.nil? ? 0 : calculate_distance(last_city, city))
     puts "  %10s | x:%.05i, y:%.05i | %.10i | %.10i" % [city.name, city.x, city.y, distance+=distance_between_cities, distance_between_cities]
